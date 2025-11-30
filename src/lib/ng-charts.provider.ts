f875c544b6b4124b11b236f2ff90be4072a48e97
siarhei.shchypanau <siarhei.shchypanau@gmail.com>
2025-11-30 03:57:07 +0200

    Add DeepPartial utility type to ng-charts.provider.ts
    - Introduced a custom DeepPartial type to enhance type safety for chart.js configurations.
    - This utility type allows for partial object structures, improving flexibility in configuration management.

--- src/lib/ng-charts.provider.ts
+++ src/lib/ng-charts.provider.ts
@@ -4,8 +4,16 @@ import {
   Defaults,
   registerables as defaultRegisterables,
 } from 'chart.js';
-import { DeepPartial } from 'chart.js/dist/types/utils';
 import { merge } from 'lodash-es';
+
+// DeepPartial utility type (not exported from chart.js)
+type DeepPartial<T> = T extends Function
+  ? T
+  : T extends Array<infer U>
+    ? Array<DeepPartial<U>>
+    : T extends object
+      ? { [P in keyof T]?: DeepPartial<T[P]> }
+      : T | undefined;
 
 export const NG_CHARTS_CONFIGURATION =
   new InjectionToken<NgChartsConfiguration>('Configuration for ngCharts');
